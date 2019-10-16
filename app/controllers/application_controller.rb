759b821291396267eb2f62574f40b89d009c812b
Sunny Asar <ring2rings93@gmail.com>
2019-10-16 15:08:58 +0100

    authentication with devise and headNav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :sanitize_data, if: :devise_controller?
+
+  private
+
+  def sanitize_data
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:acccount_update, keys: [:first_name, :last_name])
+  end
 end
--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -7,7 +7,7 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
 const Hello = props => (
-  <div>Hello {props.name}!</div>
+  <div>Hello {props.user.first_name}!</div>
 )
 
 Hello.defaultProps = {
